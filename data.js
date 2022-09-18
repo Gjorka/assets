29dfaa4a57022e25ee179a145c411ee8cde6b347
Gjorka <113481480+Gjorka@users.noreply.github.com>
2022-09-18 03:56:03 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,13 @@
+var data = 
+[
+    {
+        "title":"Script Prank 1",
+        "unduh":"belum ada",
+        "date":"Kam, 15 Sep"
+    },
+    {
+        "title":"Script Prank 2",
+        "unduh":"belum ada",
+        "date":"Min, 18 Sep"
+    }
+]
